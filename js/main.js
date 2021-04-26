25ea126c615e71dfcde9970123a7fd1912802be8
Adrian Garcia <agarcia@inove.local>
2021-04-26 18:58:52 -0500

    changes 0.025

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(".swiper-slide").mouseenter(function(e){
-    $(this).addClass('hover');
+    $(".swiper-slide").addClass('hover');
     $('.btn-buy').addClass('see');
  });
 
  $('.btn-buy').mouseenter(function(e){
-    $(this).addClass('see');
+    $('.btn-buy').addClass('see');
     $(".swiper-slide").addClass('hover');
  });
 
  $(".swiper-slide").mouseleave(function(e){
-    $(this).removeClass('hover');
+    $(".swiper-slide").removeClass('hover');
     $('.btn-buy').removeClass('see');
  });
 
- /* $('.btn-buy').mouseleave(function(e){
-    $(this).addClass('hover');
-    $('.btn-buy').addClass('see');
- }); */
+ $('.btn-buy').on('click', function(e){
+    $(".swiper-slide").removeClass('hover');
+    $('.btn-buy').removeClass('see');
+ });
